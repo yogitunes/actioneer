86ef7838287d6a13c03800eb9ed8f6807dfccb86
Daniel Staudigel <dstaudigel@gmail.com>
2015-02-22 12:48:08 -0800

    Initial commit.

---
+++ lib/tasks/actioneer_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :actioneer do
+#   # Task goes here
+# end
---
+++ test/actioneer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ActioneerTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, Actioneer
+  end
+end
